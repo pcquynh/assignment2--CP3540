ea055a39d4ac54c98d7a5becc807db6f4597a8e5
pcquynh <pcquynh@mun.ca>
2022-05-24 19:10:13 -0230

    Upload image function

--- back-end/src/server.mjs
+++ back-end/src/server.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import { MongoClient, ObjectId } from "mongodb";
 import path from "path";
 import { fileURLToPath } from "url";
+import multer from "multer";
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -10,6 +11,41 @@ app.use(express.static(path.join(__dirname, "/build")));
 app.use(express.json());
 
 const client = new MongoClient("mongodb://localhost:27017");
+
+const imageFilter = function (req, file, cb) {
+  // Accept images only
+  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+    req.fileValidationError = "Only image files are allowed!";
+    return cb(new Error("Only image files are allowed!"), false);
+  }
+  cb(null, true);
+};
+
+//File uploading
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "/build/images"));
+  },
+
+  // By default, multer removes file extensions so let's add them back
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+var upload = multer({
+  storage: storage,
+  fileFilter: imageFilter,
+}).single("poster");
+
+app.use(upload);
+
+//Make uploads folder to static
+
+app.use("/images", express.static("images"));
 
 app.post("/api/addMovie", async (req, res) => {
   try {
@@ -23,26 +59,35 @@ app.post("/api/addMovie", async (req, res) => {
   }
 });
 
+app.post("/api/upload", (req, res) => {
+  upload(req, res, (error) => {
+    if (error) {
+      console.log("first err", error);
+      res.send({
+        msg: error,
+      });
+    } else {
+      if (req.file == undefined) {
+        console.log("Error: No File Selected!");
+        res.send({
+          msg: "Error: No File Selected!",
+        });
+      } else {
+        console.log("File Uploaded!");
+        res.send({
+          msg: "File Uploaded!",
+          file: `images/${req.file.filename}`,
+        });
+      }
+    }
+  });
+});
+
 app.get("/api/data", async (req, res) => {
   try {
     await client.connect();
     const db = client.db("movies");
     const movieInfo = await db.collection("mymovies").find({}).toArray();
-    res.status(200).json(movieInfo);
-    client.close();
-  } catch (error) {
-    res.sendStatus(500);
-  }
-});
-
-app.get("/api/data/:id", async (req, res) => {
-  let id = req.params.id;
-  try {
-    await client.connect();
-    const db = client.db("movies");
-    const movieInfo = await db
-      .collection("mymovies")
-      .findOne({ _id: new ObjectId(id) });
     res.status(200).json(movieInfo);
     client.close();
   } catch (error) {
